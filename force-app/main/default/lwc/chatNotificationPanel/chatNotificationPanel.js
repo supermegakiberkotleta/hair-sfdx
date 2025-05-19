c804fff3deb82edf722c4cfc6dbdf424dd8367ee
vlad medvedev <vmedvedev135@gmail.com>
2025-05-19 16:50:23 +0300

    [hair][medvedev]: добавлен функционал уведомлений

---
+++ force-app/main/default/lwc/chatNotificationPanel/chatNotificationPanel.js
@@ -0,0 +1,63 @@
+import { LightningElement, track, wire } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/empApi';
+import USER_ID from '@salesforce/user/Id';
+import chatSound from '@salesforce/resourceUrl/chat_sound';
+import getRecordName from '@salesforce/apex/ChatNotificationHelper.getRecordName';
+
+export default class ChatNotificationPanel extends LightningElement {
+    @track messages = [];
+    channelName = '/event/Chat_Message__e';
+    subscription = null;
+    soundUrl = chatSound;
+
+    connectedCallback() {
+        this.subscribeToChannel();
+    }
+
+    subscribeToChannel() {
+        subscribe(this.channelName, -1, (response) => {
+            const evt = response.data.payload;
+            if (evt.OwnerId__c === USER_ID) {
+                this.getRecordName(evt);
+            }
+        });
+    }
+
+    getRecordName(evt) {
+        getRecordName({ recordId: evt.RecordId__c })
+            .then((name) => {
+                this.showMessage(evt, name);
+            })
+            .catch(() => {
+                this.showMessage(evt, 'Неизвестный объект');
+            });
+    }
+
+    showMessage(evt, recordName) {
+        const msg = {
+            id: Date.now(),
+            text: evt.Message__c,
+            link: '/' + evt.RecordId__c,
+            name: evt.Sender__c,
+            recordName: recordName,
+            channel: evt.Channel__c
+        };
+
+        this.messages.unshift(msg);
+        this.playSound();
+
+        if (this.messages.length > 5) {
+            this.messages.pop();
+        }
+        setTimeout(() => {
+            this.messages = this.messages.filter(m => m.id !== msg.id);
+        }, 10000);
+    }
+
+    playSound() {
+        const audio = this.template.querySelector('audio');
+        if (audio) {
+            audio.play();
+        }
+    }
+}
